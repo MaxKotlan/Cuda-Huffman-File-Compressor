584a0eaf68bd61af502bcc8953188ab39e3d5ebc
max <max.maxkot@gmail.com>
2020-02-27 22:13:51 -0500

    Inital Commit

---
+++ Compress.cu
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <cuda.h>
+#include <time.h>
+
+#define MAX_CUDA_THREADS_PER_BLOCK 1024
+
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess) 
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
+
+int main(int argc, char** argv){
+    printf("Hello World!");
+}
