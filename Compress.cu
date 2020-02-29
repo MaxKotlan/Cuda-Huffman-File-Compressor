f9717f90fe5f02c41ec6172445de5ba2145eda3a
max <max.maxkot@gmail.com>
2020-02-28 19:34:53 -0500

    sorts in descending order

--- Compress.cu
+++ Compress.cu
@@ -13,18 +13,41 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
    }
 }
 
+struct CharacterFrequency{
+    char character = 0;
+    int frequency = 0;
+};
+
+int cmp(const void *a, const void *b){
+    return ((CharacterFrequency *)b)->frequency - ((CharacterFrequency *)a)->frequency;
+}
+
 int main(int argc, char** argv){
     FileContents f = LoadFile("lotr.txt");
     //printFileContents(f);
     printf("\nFile is %ld bytes long\n", f.length);
 
-    int hashmap[256];
-    for (int i = 0; i < 256; i++)
-        hashmap[i] = 0;
+    //int hashmap[256];
+    CharacterFrequency hashmap[256];
+
+
+
     for (int i = 0; i < f.length; i++){
-        hashmap[(int)f.buffer[i]]++;
+        char letter = f.buffer[i];
+        hashmap[letter].character=letter;
+        hashmap[letter].frequency++;
     }
 
+
+    qsort(hashmap, 256, sizeof(CharacterFrequency), cmp);
+
+    for (int i = 0; i < 256; i++){
+        if (hashmap[i].frequency > 0){
+            printf(" %c %d \n", hashmap[i].character, hashmap[i].frequency);
+        }
+    }
+
+    /*
     int bytesUsed = 0;
     int columnwidth = 15;
     for (int i = 0; i < 256; i++){
@@ -33,7 +56,7 @@ int main(int argc, char** argv){
             printf("%c: %d, ", i, hashmap[i]);
             if (bytesUsed%10 == 0) printf("\n");
         }
-    }
+    }*/
 
-    printf("\n\nElements in Hashmap Used: %d / 256\n", bytesUsed);
+    //printf("\n\nElements in Hashmap Used: %d / 256\n", bytesUsed);
 }
